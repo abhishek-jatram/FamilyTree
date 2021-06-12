83fc3e159939c571914e17f31f9a83873d3e5fd8
abhishek-jatram <jatramabh@gmail.com>
2021-06-12 17:48:58 +0530

    added interactive version of the tree

---
+++ data.js
@@ -0,0 +1,232 @@
+var treeData = {
+  "children": [
+    {
+      "gender": "M", 
+      "name": "Laxmiah", 
+      "children": [
+        {
+          "gender": "M", 
+          "name": "Krishnamurthy", 
+          "children": [
+            {
+              "gender": "M", 
+              "name": "Padmaraj", 
+              "children": [
+                {
+                  "gender": "M", 
+                  "name": "Akshay", 
+                  "children": [
+                    {
+                      "gender": "F", 
+                      "name": "Aadya"
+                    }
+                  ]
+                }, 
+                {
+                  "gender": "M", 
+                  "name": "Akhil"
+                }
+              ]
+            }, 
+            {
+              "gender": "M", 
+              "name": "Vasudev", 
+              "children": [
+                {
+                  "gender": "M", 
+                  "name": "SaiKrishna"
+                }, 
+                {
+                  "gender": "M", 
+                  "name": "Harsha"
+                }
+              ]
+            }, 
+            {
+              "gender": "F", 
+              "name": "Annapurna", 
+              "children": [
+                {
+                  "gender": "M", 
+                  "name": "Balaji"
+                }, 
+                {
+                  "gender": "F", 
+                  "name": "Bandhavi"
+                }
+              ]
+            }, 
+            {
+              "gender": "F", 
+              "name": "Laxmi", 
+              "children": [
+                {
+                  "gender": "F", 
+                  "name": "Sravani"
+                }, 
+                {
+                  "gender": "F", 
+                  "name": "Sirisha"
+                }
+              ]
+            }, 
+            {
+              "gender": "M", 
+              "name": "Prakash", 
+              "children": [
+                {
+                  "gender": "M", 
+                  "name": "Kushal"
+                }, 
+                {
+                  "gender": "F", 
+                  "name": "Neha"
+                }
+              ]
+            }
+          ]
+        }, 
+        {
+          "gender": "M", 
+          "name": "Venkatesh", 
+          "children": [
+            {
+              "gender": "M", 
+              "name": "Govindraj", 
+              "children": [
+                {
+                  "gender": "M", 
+                  "name": "Anirudh"
+                }, 
+                {
+                  "gender": "F", 
+                  "name": "Keerthi"
+                }
+              ]
+            }, 
+            {
+              "gender": "F", 
+              "name": "Laxmi", 
+              "children": [
+                {
+                  "gender": "F", 
+                  "name": "Supriya"
+                }, 
+                {
+                  "gender": "F", 
+                  "name": "Vaishnavi"
+                }, 
+                {
+                  "gender": "F", 
+                  "name": "Jyothika"
+                }
+              ]
+            }
+          ]
+        }, 
+        {
+          "gender": "F", 
+          "name": "Padmavathi", 
+          "children": [
+            {
+              "gender": "M", 
+              "name": "Jayaprakash"
+            }, 
+            {
+              "gender": "M", 
+              "name": "Venkatesh"
+            }, 
+            {
+              "gender": "F", 
+              "name": "Rani"
+            }
+          ]
+        }, 
+        {
+          "gender": "F", 
+          "name": "Vijayamma", 
+          "children": [
+            {
+              "gender": "F", 
+              "name": "Roopa"
+            }, 
+            {
+              "gender": "M", 
+              "name": "Naveen"
+            }, 
+            {
+              "gender": "M", 
+              "name": "Naresh"
+            }
+          ]
+        }, 
+        {
+          "gender": "M", 
+          "name": "Srinivas", 
+          "children": [
+            {
+              "gender": "M", 
+              "name": "Deepak", 
+              "children": [
+                {
+                  "gender": "F", 
+                  "name": "Tanvi"
+                }, 
+                {
+                  "gender": "M", 
+                  "name": "Abhiram"
+                }
+              ]
+            }, 
+            {
+              "gender": "F", 
+              "name": "Annapurna", 
+              "children": [
+                {
+                  "gender": "F", 
+                  "name": "Sukrithi"
+                }, 
+                {
+                  "gender": "M", 
+                  "name": "Suchin"
+                }
+              ]
+            }
+          ]
+        }, 
+        {
+          "gender": "F", 
+          "name": "Meenakshi", 
+          "children": [
+            {
+              "gender": "F", 
+              "name": "Rajini"
+            }, 
+            {
+              "gender": "F", 
+              "name": "Swathi"
+            }, 
+            {
+              "gender": "M", 
+              "name": "Sumath"
+            }
+          ]
+        }, 
+        {
+          "gender": "M", 
+          "name": "Ananth", 
+          "children": [
+            {
+              "gender": "M", 
+              "name": "Abhishek"
+            }, 
+            {
+              "gender": "F", 
+              "name": "Shravya"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
---
+++ generate_tree_data.py
@@ -0,0 +1,29 @@
+from generate_tree import FamilyTree
+import json
+
+ftree = FamilyTree('data.csv')
+
+def pre_order_traversal(node_id):
+    node_data = dict()
+    if node_id in ftree.data.keys():
+        node_data['name'] = ftree.data[node_id]['name']
+        node_data['gender'] = ftree.data[node_id]['gender']
+    
+    if node_id in ftree.tree.keys():
+        node_data['children'] = list()
+        for child_id in ftree.tree[node_id]:
+            node_data['children'].append(pre_order_traversal(child_id))
+    return node_data
+
+tree_data = pre_order_traversal(0)
+#json_data = json.dumps(tree_data)
+#print(json_data)
+with open('data.js','w') as file:
+    json.dump(tree_data, file, indent=2, separators=(', ', ': '), sort_keys=False)
+
+lines = ['var treeData = ']
+with open('data.js', 'r') as file:
+    lines += file.readlines()
+
+with open('data.js', 'w') as file:
+    file.writelines(lines)
